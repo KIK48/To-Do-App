a94ce43ea74d3d152b98288e5a3b96cb9885fa8a
KIK48 <kikeperezalvarez@gmail.com>
2025-04-01 22:03:18 -0600

    Initial Commit

---
+++ To-Do-App/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  To-Do-App
+//
+//  Created by Kike Perez Alvarez on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ To-Do-AppTests/To_Do_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  To_Do_AppTests.swift
+//  To-Do-AppTests
+//
+//  Created by Kike Perez Alvarez on 4/1/25.
+//
+
+import Testing
+
+struct To_Do_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
